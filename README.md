3e4fd1a0e3776fcec92deab965027c5ec6399feb
tegansnyder <tsnyder@tegdesign.com>
2015-08-18 20:45:55 -0500

    intial add of library

--- README.md
+++ README.md
@@ -1,2 +1,185 @@
 # idiorm-datatable-json-bridge
-A bridge between Idiorm ORM and the popular Datatables javascript library.
+
+A bridge between [Idiorm](https://github.com/j4mie/idiorm) ORM and the popular [DataTables](https://datatables.net) javascript library that aims to abstract some of the pain points in mapping your database data to the JSON data DataTables expects.
+
+---------------
+
+### Installation
+This package is best loaded using composer and supports PHP versions > 5.2. To install into your project, add these lines to your composer.json:
+```json
+"require": {
+    "tegansnyder/idiorm-datatable-json-bridge": "*"
+}
+```
+
+---------------
+
+### Usage
+Using this library requires you to replace any instance of the `ORM` class with `OrmDatatableBridge` in cases where you wish to retrieve DataTables formated JSON. It also requires you use `get_datatable` in place of `find_many`, `find_one`, or `find_array` when grabbing result sets.
+
+##### Example:
+Here is a very simple example showing you can use the same Idorim features you have before.
+
+```php
+$datatable_json = ORMDatatableBridge::for_table('products')
+->where(array(
+    'name'	=> 'Test Product 1',
+    'price'	=> 23
+))
+->get_datatable();
+
+echo $datatable_json;
+```
+Returns:
+```json
+{
+    "recordsTotal": 1,
+    "recordsFiltered": 1,
+    "data": [
+        {
+            "id": "1",
+            "name": "Test Product 1",
+            "price": "23",
+            "weight": "12lbs"
+        }
+    ]
+}
+```
+
+##### Hidden DT Columns Example:
+Hidden data and ID columns can be set to access the data in JQuery later. Reference: http://datatables.net/manual/server-side#Returned-data
+
+```php
+$datatable_json = ORMDatatableBridge::for_table('products')
+->get_datatable(
+	array(
+		'DT_RowId' => array(
+			'type' => 'dynamic',
+			'key'  => 'products_'
+		),
+		'DT_RowData' => array(
+			'weight' => 'total_weight_{{weight}}',
+			'price'  => '{{price}}'
+		)
+	)
+);
+```
+Returns:
+```json
+{
+    "recordsTotal": 3,
+    "recordsFiltered": 3,
+    "data": [
+        {
+            "id": "1",
+            "name": "Test Product 1",
+            "price": "23",
+            "weight": "12lbs",
+            "DT_RowData": {
+                "weight": "total_weight_12lbs",
+                "price": "23"
+            }
+        },
+        {
+            "id": "2",
+            "name": "Test Product 2",
+            "price": "44",
+            "weight": "66lbs",
+            "DT_RowData": {
+                "weight": "total_weight_66lbs",
+                "price": "44"
+            }
+        },
+        {
+            "id": "3",
+            "name": "Test Product 3",
+            "price": "100",
+            "weight": "205lbs",
+            "DT_RowData": {
+                "weight": "total_weight_205lbs",
+                "price": "100"
+            }
+        }
+    ]
+}
+```
+
+##### Advanced Example:
+If you are like me you will find yourself needing to wrap columns of data in custom html. You might also want to add some dynamic columns and add buttons to your datatables for editing a row or removing a row. Below is an example of some advanced options you can pass the `get_datatable` method.
+
+```php
+$datatable_json = ORMDatatableBridge::for_table('products')
+->where('id', 1)
+->get_datatable(
+	array(
+		'DT_RowId' => array(
+			'type' => 'dynamic',
+			'key'  => 'id',
+			'prepend' => 'id_'
+		),
+		'DT_RowData' => array(
+			'weight' => 'ship_weight_{{weight}}'
+		),
+		'dynamic_columns' => array(
+			array(
+				'key' => 'edit',
+				'column_template' => '<a href="edit.php?id={id}}">Edit</a>'
+			),
+			array(
+				'key' => 'delete',
+				'column_template' => '<a href="/delete.php?id={{id}}">Delete</a>'
+			)
+		),
+		'wrap_columns' => array(
+			array(
+				'key' => 'id',
+				'column_template' => '<a href="/view.php?id={{id}}">{{id}}</a>'
+			),
+			array(
+				'key' => 'name',
+				'column_template' => '<a href="/view.php?id={{id}}">{{name}}</a>'
+			)
+		),
+		'wrap_all' => array(
+			'columns' => '<section class="datatable-column">{{column_data}}</section>'
+		)
+	)
+);
+```
+
+Returns:
+```json
+{
+    "recordsTotal": 1,
+    "recordsFiltered": 1,
+    "data": [
+        {
+            "id": "<section class=\"datatable-column\"><a href=\"/view.php?id=1\">1</a></section>",
+            "name": "<section class=\"datatable-column\"><a href=\"/view.php?id=<a href=\"/view.php?id=1\">1</a>\">Test Product 1</a></section>",
+            "price": "<section class=\"datatable-column\">23</section>",
+            "weight": "<section class=\"datatable-column\">12lbs</section>",
+            "DT_RowId": "id_1",
+            "DT_RowData": {
+                "weight": "ship_weight_12lbs"
+            },
+            "edit": "<section class=\"datatable-column\"><a href=\"edit.php?id={id}}\">Edit</a></section>",
+            "delete": "<section class=\"datatable-column\"><a href=\"/delete.php?id=1\">Delete</a></section>"
+        }
+    ]
+}
+```
+
+---------------
+
+### Contributing
+I'm happy to accept PR's that add additional functionality and fix bugs. I will do my best evaluate them prior to merging. The license is released under a [BSD license](http://en.wikipedia.org/wiki/BSD_licenses) similar to [Idiorm](https://github.com/j4mie/idiorm).
+
+
+---------------
+
+### Links
+
+[DataTables](https://datatables.net)
+[Idorim](https://github.com/j4mie/idiorm)
+
+Thanks to all the contributors of both projects!
---
+++ data_bridge.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * ORMDatatableBridge
+ * 
+ * A bridge between Idiorm ORM and the popular Datatables javascript library
+ * that provides the ability to quickly generated the required JSON for
+ * using Datatable AJAX tables.
+ *
+ * https://github.com/tegansnyder/idiorm-datatable-json-bridge
+ *
+ * BSD Licensed.
+ *
+ * Copyright (c) 2015, Tegan Snyder
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author   Tegan Snyder <tsnyder@tegdesign.com>
+ */
+class ORMDatatableBridge extends ORM {
+
+    /**
+     * @param  null|integer $id
+     * @return Model
+     */
+    public function get_datatable($options = array()) {
+    	$results = $this->_create_json(parent::find_array(), $options);
+    	return $results;
+    }
+
+    /**
+     * Method to create an instance of the model class
+     * associated with this wrapper and populate
+     * it with the supplied Idiorm instance.
+     *
+     * @param  array $data
+     * @param  array $options
+     * @return string
+     */
+    protected function _create_json($data, $options) {
+        if ($data === false) {
+            return false;
+        }
+
+        $json = array();
+        $json['recordsTotal'] = count($data);
+        $json['recordsFiltered'] = count($data);
+        $json['data'] = array();
+
+        $x = 0;
+        foreach ($data as $row) {
+
+        	// lets see if we have a row id we are using
+        	if (isset($options['DT_RowId']['type'])) {
+        		if ($options['DT_RowId']['type'] == 'fixed') {
+            		// fixed keys will most likely not be used as they increment by the x
+            		// value of the loop, the ideal identifier would be dynamic
+        			if (isset($options['DT_RowId']['key'])) {
+        				$row['DT_RowId'] = $options['DT_RowId']['key'] . $x;
+        			} else {
+        				// @todo exception handler
+        			}
+        		} elseif ($options['DT_RowId']['type'] == 'dynamic') {
+        			// dynmaic keys increment by the value of the key and the value
+        			// or another identifier like an id column in the dataset
+        			if (isset($options['DT_RowId']['key'])) {
+        				if (isset($row[$options['DT_RowId']['key']])) {
+        					$row['DT_RowId'] = $row[$options['DT_RowId']['key']];
+        				} else {
+        					// @todo exception handler
+        				}
+        			}
+        		}
+
+        		if (isset($options['DT_RowId']['prepend'])) {
+            		if (!empty($row['DT_RowId'])) {
+            			$row['DT_RowId'] = $options['DT_RowId']['prepend'] . $row['DT_RowId'];
+            		}
+        		}
+
+
+        	}
+        	
+        	/**
+        	 * http://datatables.net/manual/server-side#Returned-data
+        	 * Add the data contained in the object to the row using the jQuery data() method to set the data, 
+        	 * which can also then be used for later retrieval (for example on a click event).
+        	 */
+        	if (isset($options['DT_RowData'])) {
+        		if (is_array($options['DT_RowData'])) {
+        			foreach ($options['DT_RowData'] as $key => $val) {
+
+        				// lets see if we are including row data via template
+        				$templated_vals = $this->getTemplatedValues($val);
+						foreach ($templated_vals as $t_val) {
+        					if (isset($row[$t_val])) {
+        						$val = str_replace('{{' . $t_val . '}}', $row[$t_val], $val);
+        					}
+        				}
+        				$row['DT_RowData'][$key] = $val;
+        			}
+        		}
+        	}
+
+        	/**
+        	 * the ability to add dynamic columns to the result set is very important
+        	 * for instance you may want a column called `actions` that has an edit or a delete button
+        	 * this module provides a very simple way to add these
+        	 */
+        	if (isset($options['dynamic_columns'])) {
+        		foreach ($options['dynamic_columns'] as $col) {
+        			if (isset($col['key']) && isset($col['column_template'])) {
+        				$templated_vals = $this->getTemplatedValues($col['column_template']);
+        				foreach ($templated_vals as $t_val) {
+        					if (isset($row[$t_val])) {
+        						$col['column_template'] = str_replace('{{' . $t_val . '}}', $row[$t_val], $col['column_template']);
+        					}
+        				}
+        				$row[$col['key']] = $col['column_template'];
+        			} else {
+        				// @todo exception handler
+        			}
+        		}
+        	}
+
+
+        	// the ability to wrap columns dyanamically with html
+        	if (isset($options['wrap_columns'])) {
+        		foreach ($options['wrap_columns'] as $col) {
+        			if (isset($col['key']) && isset($col['column_template'])) {
+        				$templated_vals = $this->getTemplatedValues($col['column_template']);
+        				foreach ($templated_vals as $t_val) {
+        					if (isset($row[$t_val])) {
+        						$col['column_template'] = str_replace('{{' . $t_val . '}}', $row[$t_val], $col['column_template']);
+        					} elseif ($t_val == 'column_data') {
+        						$col['column_template'] = str_replace('{{' . $t_val . '}}', $row[$col['key']], $col['column_template']);
+        					}
+
+        				}
+        				$row[$col['key']] = $col['column_template'];
+        			} else {
+        				// @todo exception handler
+        			}
+        		}
+        	}
+
+        	// ability to wrap an entire column with custom HTML
+        	if (isset($options['wrap_all']['columns'])) {
+        		$new_row = array();
+        		foreach ($row as $key => $val) {
+        			if ($key == 'DT_RowId' || $key == 'DT_RowData') {
+        				continue;
+        			}
+        			$new_row[$key] = str_replace('{{column_data}}', $val, $options['wrap_all']['columns']);
+        		}
+        		foreach ($new_row as $key => $val) {
+        			$row[$key] = $val;
+        		}
+        	}
+        	
+        	array_push($json['data'], $row);
+
+        	$x = $x + 1;
+        }
+
+        $json = json_encode($json);
+
+        return $json;
+    }
+
+    /**
+     * @param  string $str
+     * @return array
+     */
+    private function getTemplatedValues($str) {
+    	$templated_vals = array();
+    	// extract a list of strings between {{tags}}
+    	preg_match_all('/{{.*?}}/is', $str, $matches);
+    	$matches = $matches[0];
+    	foreach ($matches as $m) {
+    		$m = str_replace('{{', '', $m);
+    		$m = str_replace('}}', '', $m);
+    		$templated_vals[] = $m;
+    	}
+    	return $templated_vals;
+    }
+
+    /**
+     * Factory method, a repeat of content in parent::for_table, so that
+     * created class is ORMDatatableBridge, not ORM
+     *
+     * @param  string $table_name
+     * @param  string $connection_name
+     * @return ORMDatatableBridge
+     */
+    public static function for_table($table_name, $connection_name = parent::DEFAULT_CONNECTION) {
+        self::_setup_db($connection_name);
+        return new self($table_name, array(), $connection_name);
+    }
+
+}
